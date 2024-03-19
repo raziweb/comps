946d07dff21c82eab3747c49c502be0c92641d15
Razi Shahid <mailrazi786@gmail.com>
2024-03-20 00:18:00 +0530

    Add table component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import AccordionPage from "./pages/AccordionPage";
 import ButtonPage from "./pages/ButtonPage"
 import Sidebar from "./components/Sidebar";
 import ModalPage from "./pages/ModalPage";
+import TablePage from "./pages/TablePage";
 
 function App() {
   return (
@@ -22,6 +23,9 @@ function App() {
         </Route>
         <Route path="/modal">
           <ModalPage />
+        </Route>
+        <Route path="/table">
+          <TablePage />
         </Route>
       </div>
     </div>
